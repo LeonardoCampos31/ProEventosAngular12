ff56c6979c17429f3bd9d70642574f7ed9756699
Leonardo Teodoro <leonardo_campos@outlook.com>
2022-01-28 23:41:16 -0300

    Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,39 +13,22 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        IEnumerable<Evento> _evento = new Evento[]{
-                    new Evento(){ EventoID = 1,
-                    Tema = "Angular .net 5",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToLongDateString(),
-                    ImagemUrl = "foto.png"
-                },
-                new Evento(){ EventoID = 2,
-                    Tema = "Angular 11 e .net 5",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToLongDateString(),
-                    ImagemUrl = "foto2.png"
-                }
-            };
-
-        public EventoController()
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
         {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> Get(int id)
         {
-            return _evento.Where(evento => evento.EventoID == id);
+            return _context.Eventos.Where(evento => evento.EventoID == id);
         }
 
     }
--- Back/src/ProEventos.API/Models/Evento.cs
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace ProEventos.API.Models
 {
     public class Evento
     {
         public int EventoID { get; set; }
         public string Local { get; set; }
-        public string DataEvento { get; set; }
+        public DateTime DataEvento { get; set; }
         public string Tema { get; set; }
         public int QtdPessoas { get; set; }
         public string Lote { get; set; }

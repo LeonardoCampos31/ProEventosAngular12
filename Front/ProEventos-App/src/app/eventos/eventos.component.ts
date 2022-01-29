eb50aa25dcfa82a2b8db997120275e057174cc5d
Leonardo Teodoro <leonardo_campos@outlook.com>
2022-01-29 13:15:48 -0300

    Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImage = 150;
+  showImage = false;
+  isCollapsed = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(){
+      return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ?  this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +40,13 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public toogleImage(){
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
+      response => this.eventos = this.eventosFiltrados = response,
       error => console.log(error)
     );
   }
